ac1a1fad9fee7157b47be43a702d9ebe28af3dab
Philipp <philipp@Wearables-MacBook-Pro.local>
2022-03-10 15:53:16 +0100

    Add Blank Svelte Project

---
+++ website/src/routes/index.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+	import { getAllPosts } from '../../static/a';
+
+export async function get() {
+	return { body: await getAllPosts() };
+}
+</script>
+
+<script>
+
+let body;
+
+</script>
+
+{body}
+
+<!-- <ul>
+	{#each body as file}
+		<li>{file}</li>
+	{/each}
+</ul> -->
+
+
