eca3359f7b8d649df822a6523451e9d2335b0401
Philipp <p6gb@posteo.net>
2022-04-19 14:49:27 +0200

    Implement Swiper

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,34 @@
-let glider;
+let swiper;
 
 window.addEventListener("load", () => {
   console.log("main.js");
-  initGlider();
+  initSwiper();
   initThumbnailEventListeners();
 });
 
-function initGlider() {
-  glider = new Glider(document.querySelector(".js-glider"), {
-    slidesToScroll: 1,
-    slidesToShow: 1,
-    draggable: true,
-    dots: ".dots",
-    arrows: {
-      prev: ".glider-prev",
-      next: ".glider-next",
+function initSwiper() {
+  // glider = new Glider(document.querySelector(".js-glider"), {
+  //   slidesToScroll: 1,
+  //   slidesToShow: 1,
+  //   draggable: true,
+  //   dots: ".dots",
+  //   arrows: {
+  //     prev: ".glider-prev",
+  //     next: ".glider-next",
+  //   },
+  // });
+  swiper = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
   });
+
   document
     .querySelector(".js-slideshow-close-button")
     .addEventListener("click", () => {
@@ -30,7 +42,10 @@ function initThumbnailEventListeners() {
     thumbnail.addEventListener("click", (ev) => {
       document.body.classList.add("slideshow-open");
       console.log(ev.target.parentElement.dataset.index);
-      glider.scrollItem(ev.target.parentElement.dataset.index, true);
+      const index = ev.target.parentElement.dataset.index;
+      swiper.slideTo(index, 0);
+      console.log(swiper.activeIndex);
+      // glider.scrollItem(ev.target.parentElement.dataset.index, true);
     })
   );
 }
